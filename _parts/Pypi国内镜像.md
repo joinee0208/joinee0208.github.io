6bb7b2de270abb1181f1b77cd22b47a1ebeade86
joinee0208 <364595444@qq.com>
2020-12-17 18:25:31 +0800

    update

---
+++ _parts/Pypi国内镜像.md
@@ -0,0 +1,48 @@
+---
+layout: part
+title: Pypi国内镜像
+original: true
+typora-root-url: ..\
+---
+
+### 国内pypi镜像
+
+#### 东软信息学院
+
+https://mirrors.neusoft.edu.cn/pypi/web/simple/
+
+#### 豆瓣
+
+https://pypi.douban.com/simple
+
+#### 阿里云
+
+https://mirrors.aliyun.com/pypi/simple/
+
+#### 中国科学技术大学
+
+https://pypi.mirrors.ustc.edu.cn/simple/
+
+#### 清华大学
+
+https://pypi.tuna.tsinghua.edu.cn/simple
+
+#### 华中理工大学
+
+https://pypi.hustunique.com/
+
+#### 山东理工大学
+
+https://pypi.sdutlinux.org/
+
+#### 中国科学技术大学
+
+https://pypi.mirrors.ustc.edu.cn/
+
+#### 搜狐镜像
+
+https://mirrors.sohu.com/Python/
+
+### 使用国内镜像安装命令模版
+
+pip install --install PyQt5 -i https://pypi.douban.com/simple
---
+++ assets/js/mouse-text.js
@@ -0,0 +1,29 @@
+<!--鼠标点击文本-->
+
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("上班996","下班ICU","❤","[]~(￣▽￣)~*","❤","我爱学习","学习使我快乐","❤","o((>ω< ))o","❤");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+    });
+});
